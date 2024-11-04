32025252582541353cd9cd994463bafa8eef4f1b
gsaden <aden@getsolar.ai>
2024-11-04 23:27:55 +0800

    update

--- nvim/after/plugin/conform.lua
+++ nvim/after/plugin/conform.lua
@@ -7,6 +7,8 @@ require("conform").setup({
 		javascriptreact = { "prettier" },
 		typescriptreact = { "prettier" },
 		yaml = { "yamlfmt" },
+        json = { "prettier"},
+        jsonc = { "prettier"}
 	},
 	format_on_save = {
 		lsp_format = "fallback",
